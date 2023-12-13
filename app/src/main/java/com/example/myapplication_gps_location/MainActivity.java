3a01c5f737d5f72091eced854a86927c34291e6c
hgtmay58 <hgtmay58@gmail.com>
2023-12-13 11:36:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication_gps_location/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.myapplication_gps_location;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+TextView t;
+LocationManager locationManager;
+Location location;
+double mLatitude = 0;
+double mLongitude = 0;
+String provider;
+MainActivity mactivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mactivity=this;
+        t=(TextView)findViewById(R.id.t1);
+        //
+        if (ActivityCompat.checkSelfPermission(mactivity, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(mactivity, new String[]{ACCESS_FINE_LOCATION}, 12);
+
+        }
+        else
+            initlocation();
+
+
+
+
+
+
+    }
+//
+//
+@Override
+public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+{
+    if(requestCode==12)
+    {
+        if(grantResults.length>0&&grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+            initlocation();
+        }
+        else
+            return;
+    }
+}
+ //
+  void initlocation() {
+     try {
+         // Getting LocationManager object from System Service LOCATION_SERVICE
+         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+         // Creating a criteria object to retrieve provider
+         Criteria criteria = new Criteria();
+
+         // Getting the name of the best provider
+         provider = locationManager.getBestProvider(criteria, true);
+         // Enabling  get My Location
+         // Getting Current Location From GPS
+         location = locationManager.getLastKnownLocation(provider);
+         locationManager.requestLocationUpdates(provider, 12000, 0, mactivity);
+         onLocationChanged(location);
+     }
+     catch(SecurityException e)
+     {
+         ActivityCompat.requestPermissions(mactivity, new String[]{ACCESS_FINE_LOCATION}, 12);
+     }
+
+ }
+    //
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if(location==null)
+        {
+          t.setText("尚未定位成功!");
+        }
+        else {
+            mLatitude = location.getLatitude();
+            mLongitude = location.getLongitude();
+
+            String altitiude = "Altitiude(高度): " + location.getAltitude();
+            String accuracy = "Accuracy: " + location.getAccuracy();
+            String time = "Time: " + location.getTime();
+
+            t.setText("位置座標:"+mLatitude+","+mLongitude+ "\n"
+                    + altitiude + "\n" + accuracy + "\n" + time);
+
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
